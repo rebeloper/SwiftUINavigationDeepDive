4f4ea40c9fa25279f66cdaac7307ff40d41ad730
rebeloper <therebeloper@gmail.com>
2023-02-19 12:28:57 +0200

    Initial Commit

---
+++ SwiftUINavigationDeepDive/SwiftUINavigationDeepDiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationDeepDiveApp.swift
+//  SwiftUINavigationDeepDive
+//
+//  Created by Alex Nagy on 19.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationDeepDiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
